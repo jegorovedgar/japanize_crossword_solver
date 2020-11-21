d155e9a99be16298aa0bace4c934153dd8ce4f02
Edgar Jegorv <jegorov.e@gmail.com>
2020-11-21 02:56:49 +0200

    cells sequences

--- src/models/solver.test.ts
+++ src/models/solver.test.ts
@@ -1,4 +1,5 @@
-import { getMatrixCol, getMatrixRow, getSpaceSequences } from "./solver"
+import { MatrixCellState } from "../components/game/cells/matrix-cell"
+import { getCellsSequences, getMatrixCol, getMatrixRow, getRequiredCells, getSpaceSequences } from "./solver"
 
 describe('solver', () => {
     describe('getSpaceSequences', () => {
@@ -42,6 +43,56 @@ describe('solver', () => {
         it('should return sequences for value: 10 and 10 spaces', () => {
             expect(getSpaceSequences(10, 10).length).toBe(92378)
         })
+    })
+
+    describe('getRequiredCells', () => {
+        it('should return required cells for [1, 2]', () => {
+            expect(getRequiredCells([1, 2])).toEqual([
+                [MatrixCellState.Filled, MatrixCellState.Empty],
+                [MatrixCellState.Filled, MatrixCellState.Filled],
+            ])
+        })
+
+        it('should return required cells for [1, 2, 1]', () => {
+            expect(getRequiredCells([1, 2, 1])).toEqual([
+                [MatrixCellState.Filled, MatrixCellState.Empty],
+                [MatrixCellState.Filled, MatrixCellState.Filled, MatrixCellState.Empty],
+                [MatrixCellState.Filled],
+            ])
+        })
+
+
+        it('should return required cells for [1]', () => {
+            expect(getRequiredCells([1])).toEqual([
+                [MatrixCellState.Filled],
+            ])
+        })
+
+        it('should return required cells for []', () => {
+            expect(getRequiredCells([])).toEqual([])
+        })
+    });
+
+    describe('getCellsSequences', () => {
+        it('should return sequence for [1, 1] and size: 4', () => {
+            expect(getCellsSequences([1, 1], 4)).toEqual([
+                [MatrixCellState.Empty, MatrixCellState.Filled, MatrixCellState.Empty, MatrixCellState.Filled],
+                [MatrixCellState.Filled, MatrixCellState.Empty, MatrixCellState.Empty, MatrixCellState.Filled],
+                [MatrixCellState.Filled, MatrixCellState.Empty, MatrixCellState.Filled, MatrixCellState.Empty],
+            ])
+        });
+
+
+        it('should return sequence for [1, 1] and size: 5', () => {
+            expect(getCellsSequences([1, 1], 5)).toEqual([
+                [MatrixCellState.Empty, MatrixCellState.Empty, MatrixCellState.Filled, MatrixCellState.Empty, MatrixCellState.Filled],
+                [MatrixCellState.Empty, MatrixCellState.Filled, MatrixCellState.Empty, MatrixCellState.Empty, MatrixCellState.Filled],
+                [MatrixCellState.Empty, MatrixCellState.Filled, MatrixCellState.Empty, MatrixCellState.Filled, MatrixCellState.Empty],
+                [MatrixCellState.Filled, MatrixCellState.Empty, MatrixCellState.Empty, MatrixCellState.Empty, MatrixCellState.Filled],
+                [MatrixCellState.Filled, MatrixCellState.Empty, MatrixCellState.Empty, MatrixCellState.Filled, MatrixCellState.Empty],
+                [MatrixCellState.Filled, MatrixCellState.Empty, MatrixCellState.Filled, MatrixCellState.Empty, MatrixCellState.Empty],
+            ])
+        });
     })
 
     describe('getMatrixRow', () => {
--- src/models/solver.ts
+++ src/models/solver.ts
@@ -1,7 +1,12 @@
+import { GameDefinition } from "../components/game";
+import { MatrixCellState } from "../components/game/cells/matrix-cell";
+import { GameDefinitionSequence } from "../components/game/definition";
 import { GameMatrix } from "../components/game/playground";
 
+export type DefinedMatrixCellState = MatrixCellState.Empty | MatrixCellState.Filled;
+
 export const getSpaceSequences = (value: number, spaces: number): Array<Array<number>> => {
-    if (spaces <= 1){
+    if (spaces <= 1) {
         return [[value]]
     }
     const termNumbers = new Array(value + 1).fill(null).map((_, i, arr) => value - i);
@@ -10,5 +15,39 @@ export const getSpaceSequences = (value: number, spaces: number): Array<Array<nu
     ), [])
 }
 
+export const getRequiredCells = (definition: GameDefinitionSequence[0]): Array<Array<DefinedMatrixCellState>> => {
+    return definition.map((size, i) =>
+        new Array(size).fill(null)
+            .map<DefinedMatrixCellState>(() => MatrixCellState.Filled)
+            .concat(i < definition.length - 1 ? [MatrixCellState.Empty] : [])
+    )
+}
+
+export const getCellsSequences = (definition: GameDefinitionSequence[0], totalSize: number): Array<Array<DefinedMatrixCellState>> => {
+    //   1. get array of required cells
+    const requiredCells = getRequiredCells(definition);
+    const requiredSize = requiredCells.reduce((total, cells) => total + cells.length, 0);
+
+    //   2. get spaces sequences
+    const spacesSequences = getSpaceSequences(totalSize - requiredSize, requiredCells.length + 1);
+    //   3. map spaces with cells
+    return spacesSequences.map(spaces => {
+        return spaces.reduce<Array<DefinedMatrixCellState>>((acc, spaceSize, i) => {
+            return acc.concat(
+                new Array(spaceSize).fill(null)
+                    .map<DefinedMatrixCellState>(() => MatrixCellState.Empty)
+                    .concat(requiredCells[i] || [])
+            )
+        }, [])
+    })
+}
+
 export const getMatrixRow = (matrix: GameMatrix, x: number) => matrix[x].slice()
 export const getMatrixCol = (matrix: GameMatrix, y: number) => matrix.map(row => row[y]);
+
+export const solveStep = (matrix: GameMatrix, definition: GameDefinition): GameMatrix => {
+    // 1. generate cell sequences
+    // 2. get rid of invalid sequences
+    // 3. get aligned cells
+    return [];
+}
